40367af1c923db950d1ec385d9a9623eb8e2b8bf
Mike <izardon@gmail.com>
2023-09-02 16:50:08 +0800

    Added DDDCore and Backend Modules

---
+++ dddcore/src/main/java/org/izardon/model/ValueObject.java
@@ -0,0 +1,12 @@
+package org.izardon.model;
+
+import java.io.Serializable;
+
+public abstract class ValueObject implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public ValueObject() {
+        super();
+    }
+}
